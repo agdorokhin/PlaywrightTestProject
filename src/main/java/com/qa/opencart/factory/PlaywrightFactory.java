cb33071610c7cc982b5bde5acb439df7c33c9c41
Oleksii Dorokhin <agdorokhin@gmail.com>
2023-02-20 17:25:34 +0200

    First commit

---
+++ src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
@@ -0,0 +1,42 @@
+package com.qa.opencart.factory;
+
+import com.microsoft.playwright.*;
+
+public class PlaywrightFactory {
+    Playwright playwright;
+    Browser browser;
+    BrowserContext browserContext;
+    Page page;
+
+    public Page initBrowser(String browserName) {
+        System.out.println("browser name is: " + browserName);
+
+        playwright = Playwright.create();
+
+        switch (browserName.toLowerCase()) {
+            case "chromium":
+                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                break;
+            case "firefox":
+                browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                break;
+            case "safari":
+                browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                break;
+            case "chrome":
+                browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
+                break;
+
+            default:
+                System.out.println("Please pass the right browser name...");
+                break;
+        }
+
+        browserContext = browser.newContext();
+        page = browserContext.newPage();
+        page.navigate("https://naveenautomationlabs.com/opencart/");
+
+        return page;
+
+    }
+}
---
+++ src/test/java/TestImplementation.java
@@ -0,0 +1,35 @@
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestImplementation {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupWebdriverChromeDriver() {
+//        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "D:/scratch/selenium-java/SeleniumDemoProject/src/main/resources/chromedriver.exe");
+    }
+
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void verifyGitHubTitle() {
+        driver.get("https://www.github.com");
+        assertThat(driver.getTitle(), containsString("GitHub"));
+    }
+}
